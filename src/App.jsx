1da12b287438fe4fc54c5addf73782ac87dd3c23
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-01 18:43:35 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
     <>
       <Header title="Expense Tracker" />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </>
   );
 }
