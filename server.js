fb6a6fc18b6cc8ab0dcb5802219613a470b3b37a
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-02 17:01:42 +0200

    ignore all .env files

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold
   )
 );
