7efcc1deb2c16033cedb8c97e687ef64c78bf21f
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-02 16:54:47 +0200

    added script to use enviroment variables

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import colors from "colors";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("hello"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
