ce85889135d46c85b2904c65e117237bd0c56512
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-03 17:44:00 +0200

    add transaction model

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(
-      `MongoDB connected: ${connection.connection.host}.orange.underline.bold`
+      `MongoDB connected: ${connection.connection.host}`.magenta.underline.bold
     );
   } catch (error) {
     console.log(`Error: ${error.message}`.red);
